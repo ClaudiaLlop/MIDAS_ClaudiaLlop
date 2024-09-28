59b2f64e491eb1a83dedbc3ea2e99b275b3df768
ClaudiaLlop <cllop72@gmail.com>
2024-09-28 15:46:39 +0200

    add loggins

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ if __name__ == '__main__':
 
     with app.app_context():
         db.create_all()
+        logger.info("Tables created (if they didn't exist).")
     logger.info("Service started")
     app.run(debug=True)
